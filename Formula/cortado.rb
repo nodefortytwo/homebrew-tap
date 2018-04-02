b12efe22a66ecd5d6d07c80d5f46d10b5d28797e
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-02 15:11:40 +0100

    Brew formula update for cortado version v0.1.1

--- Formula/cortado.rb
+++ Formula/cortado.rb
@@ -1,16 +1,12 @@
 class Cortado < Formula
-  desc "testing"
+  desc "Very simple S3 file editor that temporarily downloads a file from S3, opens it in vim and re-uploads it when you exit vim"
   homepage ""
-  url "https://github.com/nodefortytwo/cortado/releases/download/v0.0.3/cortado_0.0.3_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.0.3"
-  sha256 "acf0c16e9ae260fe43e819ed843806596a3cdb335916adbb740eca8a643585dd"
+  url "https://github.com/nodefortytwo/cortado/releases/download/v0.1.1/cortado_0.1.1_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.1.1"
+  sha256 "8279cf5040ea623e98dae22cc1e730fcffe080a26544720e6f7b26ff4756d342"
 
   def install
     bin.install "cortado"
-  end
-
-  def caveats
-    "who knows"
   end
 
   test do
